3c42cd57adc82b03f515dcfb0f6ffa7bfab7936e
gabeh <gabeh@reed.edu>
2022-03-11 15:58:47 -0800

    updating from lab

--- CS221_code/C++/lab7/student_list.cc
+++ CS221_code/C++/lab7/student_list.cc
@@ -10,12 +10,12 @@ struct Student {
   Student* next_;
 };
 
-void delete_list1(Student* head)  // Bad:
+void delete_list1(Student* head)  // Bad: Only deleting the pointer, should delete all elements before deleting the pointer
 {
   delete head;
 }
 
-void delete_list2(Student* head)   // Bad:
+void delete_list2(Student* head)   // Bad: Deletes the pointer and then tries to access where the pointer goes to
 {
   while(head) {
     delete head;
@@ -23,7 +23,7 @@ void delete_list2(Student* head)   // Bad:
   }
 }
 
-void delete_list3(Student* head)  // Bad:
+void delete_list3(Student* head)  // Bad: This will break when passed Null
 {
   while(head->next_) {
     delete head;
@@ -31,7 +31,7 @@ void delete_list3(Student* head)  // Bad:
   }
 }
 
-void delete_list4(Student* head)  // Bad:
+void delete_list4(Student* head)  // Bad: this function will not delete a list size of 1
 {
   while(head->next_) {
     Student* tmp = head->next_;
@@ -40,7 +40,7 @@ void delete_list4(Student* head)  // Bad:
   }
 }
 
-void delete_list5(Student* head)  // Bad?
+void delete_list5(Student* head)  // Bad? 
 {
   if (!head) {
     return;
@@ -53,7 +53,7 @@ void delete_list5(Student* head)  // Bad?
   delete head;
 }
 
-void delete_list6(Student* head)  // Bad?
+void delete_list6(Student* head)  // Bad? tries to delete an array which is not what head is
 {
   if (!head) {
     return;
@@ -66,7 +66,7 @@ void delete_list6(Student* head)  // Bad?
   delete[] head;
 }
 
-void delete_list7(Student* head)  // Bad?
+void delete_list7(Student* head)  // Bad? tmp should be a pointer to the next item, not an auto value
 {
   if (!head) {
     return;
@@ -80,7 +80,7 @@ void delete_list7(Student* head)  // Bad?
   delete head;
 }
 
-void delete_list8(Student* head) // Bad?
+void delete_list8(Student* head) // Bad? this one works
 {
   if (!head) return;
   if (!head->next_) delete head;
@@ -92,12 +92,12 @@ void delete_list8(Student* head) // Bad?
 int main()
 {
   Student* lst;
-  lst->name_ = "whoami"; // Bad:
-  delete lst;            // Bad:
-  delete_list1(lst);     // Bad:
+  lst->name_ = "whoami"; // Bad: it points to the structure, not calling for new space
+  delete lst;            // Bad: trying to delete something that hasnt been initialized
+  delete_list1(lst);     // Bad: same as above, but also lst is uninitialized
 
   lst = new Student({ "Joe", 1234, nullptr });
-  delete_list1(lst);    // Bad:
+  delete_list1(lst);    // Bad: delete list1 is broken
 
   lst = new Student({ "Joe", 1234, nullptr });
   lst = new Student({ "Jolene", 4321, nullptr });  // Bad:
