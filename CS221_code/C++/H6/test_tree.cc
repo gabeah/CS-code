135c8c41cdf2e954d769a36e8eb2dadcf7600810
gabeh <gabeh@reed.edu>
2022-03-15 10:43:58 -0700

    quick patch

--- CS221_code/C++/H6/test_tree.cc
+++ CS221_code/C++/H6/test_tree.cc
@@ -4,11 +4,13 @@
 // This testing file will construct a tree and do some operation tests
 
 #include "tree.hh"
+#include <assert.h>
 
 int main() {
 
 	Tree* a = create_tree(6,12,nullptr,nullptr);
 	Tree* b = create_tree(8,23,a,nullptr);
+	assert(b!=NULL);
 	return 0;
 
 }
